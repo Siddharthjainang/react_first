6d53c1b968d44b9c8bec6f067ff8d489c8e09618
Siddharth <jain8siddharth8@gmail.com>
2020-02-21 13:56:29 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Person from './components/person/person';
+
+function App() {
+  return (
+    <div className="App">
+     <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/person/person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Person = () => {
+    return(
+        <div>
+            person
+        </div>
+    )
+}
+export default Person;
